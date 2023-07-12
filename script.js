7a3629447522d0cfb9042fcbcbe93db18e7dbc81
darmankulova <imetoffa1@gmail.com>
2023-07-12 17:14:43 +0600

    hw 3

---
+++ script.js
@@ -0,0 +1,128 @@
+// user admin true welcome, 'user'
+// false not correct user
+// function chekUser(user){
+//     if (user==='admin'){
+//         return 'welcome'
+//     }else {
+//         return 'false not correct user'
+//     }
+// }
+//
+// console.log(chekUser('admin'))
+
+//TASK
+// today is 'day' of february
+// on february only 28 day, your day is 'day' of another month
+
+// function monthDay(day){
+//     if (day <= 1 || day >= 28){
+//         return `today is ${day} of february`
+//     }else {
+//         return 'on february only 28 day, your day is \'day\' of another month'
+//     }
+// }
+//
+// console.log(monthDay('1'))
+
+//TASK
+// 'num' is equals of 50
+// 'num' is not correct
+
+// function numFunction(num){
+//     if (num===50 ){
+//         return ` ${num} is equal of 50 `
+//     }else {
+//         return `${num} is not correct`
+//     }
+// }
+//
+// console.log(numFunction(50))
+
+//TASK
+// 'user' 'age' old, welcome
+// 'user' 'age' old, rejected
+//  function infoUser(user,age){
+//     if (user==='Nurperi', age >=18){
+//        return ` ${user} welcome`
+//     }else {
+//         return 'rejected'
+//     }
+//  }
+//
+// console.log(infoUser('Nurperi',19))
+
+//TASK
+// 'user' name is too short (min 3)
+// 'user' name is accepted
+// 'user' name is too long (max 5)
+// 'user' name is accepted
+
+// function minMax(user){
+//     if (user.length >= 3 && user.length < 6){
+//         return 'accepted'
+//     } else{
+//         return 'error'
+//     }
+// }
+//
+// console.log(minMax('Hani'))
+
+//TASK
+//'age' must be a number ('20')
+// function age(num){
+//     if (typeof num == "number" ){
+//         return `your age ${num}  `
+//     }else {
+//         return `${num} must be a number`
+//     }
+// }
+//
+// console.log(age('20'))
+
+//TASK
+// 'age' must be a string (20)
+// function userAge(age){
+//     if ( typeof age == "string"){
+//         return `your age ${age}`
+//     }else {
+//         return 'must be string'
+//     }
+// }
+//
+// console.log(userAge('20'))
+
+//TASK
+// 'age' must be older then 23
+
+// function olderNum(age){
+//     if (age>23){
+//         return `your age ${age}`
+//     }else {
+//         return  'must be older then 23'
+//     }
+// }
+// console.log(olderNum(25))
+
+//TASK
+// today is monday/tuesday/wednesday/thursday/friday weekend
+// max days of weeks 7
+
+// function dayWeeks(day){
+//     if (day === 1){
+//         return 'today is monday'
+//     } else if(day === 2){
+//         return 'today is tuesday'
+//     }else if(day === 3){
+//         return 'today is wednesday'
+//     }else if(day === 4){
+//         return 'today is thursday'
+//     } else if(day === 5){
+//         return 'today is friday'
+//     } else if(day === 6,7){
+//         return 'today is weekend'
+//     }else {
+//         return 'max days of weeks 7'
+//     }
+// }
+//
+// console.log(dayWeeks(7))
